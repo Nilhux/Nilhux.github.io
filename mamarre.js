6a5fe62db80d410b10d6c87147e328ad9943217f
nicolas ua <nicosua1422@gmail.com>
2021-10-25 20:27:37 -0500

    primer calculador super pro salu2

---
+++ mamarre.js
@@ -0,0 +1,25 @@
+const calcularPerimetro =(x,y)=>{
+    return (x+y)*2;
+}
+
+const calcularArea =(x,y)=>{
+    return x*y;
+}
+
+const calcularPerimetroCuadrado =()=>{
+    let etiqueta1= document.getElementById("valor1")
+    let etiqueta2= document.getElementById("valor2")
+    let valor1 = parseInt(etiqueta1.value);
+    let valor2 = parseInt(etiqueta2.value);
+    let perimetro = calcularPerimetro(valor1,valor2);
+    alert(`Tu perimetro es de ${perimetro} cm`)
+}
+
+const calcularAreaCuadrada =()=>{
+    let etiqueta1= document.getElementById("valor1")
+    let etiqueta2= document.getElementById("valor2")
+    let valor1 = parseInt(etiqueta1.value);
+    let valor2 = parseInt(etiqueta2.value);
+    let area = calcularArea(valor1,valor2);
+    alert(`Tu area es de ${area} cm2`)
+}
