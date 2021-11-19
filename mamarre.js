c8c5398ec0b775f7962de7b8f3fc648498061d77
nicolas ua <nicosua1422@gmail.com>
2021-11-18 20:31:15 -0500

    commit de algo bien bacano

--- mamarre.js
+++ mamarre.js
@@ -1,1 +1,12 @@
+var estudiantes =["Pedro","Luis","Alonso"];
 
+
+const agregarNombre =()=>{
+    let etiqueta = document.getElementById("lista")
+    let valor = etiqueta.value;
+    estudiantes = estudiantes.push(valor);
+
+    let etiqueta2 = document.getElementById("texto");
+    let valor2= etiqueta2.value;
+    
+}
