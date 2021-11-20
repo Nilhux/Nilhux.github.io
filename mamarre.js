e98017a78ec881cb4d7504e2d9a05f33ef054bd1
nicolas ua <nicosua1422@gmail.com>
2021-11-20 15:31:56 -0500

    le añadi una clase y una instancia, saludos a Uribe

--- mamarre.js
+++ mamarre.js
@@ -1,12 +1,67 @@
-var estudiantes =["Pedro","Luis","Alonso"];
+const calcularPrecioConDescuento = (precio, descuento)=>{
+    const porcentajeAPagar = 100 - descuento;
+    const precioAPagar = (precio * porcentajeAPagar)/100;
+    return precioAPagar
 
+}   
 
-const agregarNombre =()=>{
-    let etiqueta = document.getElementById("lista")
-    let valor = etiqueta.value;
-    estudiantes = estudiantes.push(valor);
+const funcionDescuento = ()=>{
+    const etiquetaPrecio = document.getElementById("precio");
+    const precioOriginal = etiquetaPrecio.value;
 
-    let etiqueta2 = document.getElementById("texto");
-    let valor2= etiqueta2.value;
+    const etiquetaDescuento = document.getElementById("descuento");
+    let valorDescuento = parseInt(etiquetaDescuento.value);
+
+    const cupones =[
+        "elsingueson",
+        "quintanoso",
+        "da sus"
+    ]
+    const etiquetaCupones = document.getElementById("cupones")
+    const valorCupones = etiquetaCupones.value;
+    let descuento;
     
+    switch(valorCupones){
+        case cupones[0]:
+            descuento = 5;
+            valorDescuento = valorDescuento + descuento;
+            break;          
+        case cupones[1]:
+            descuento = 15;
+            valorDescuento = valorDescuento + descuento;
+            break;
+        case cupones[2]:
+            descuento = 25;
+            valorDescuento = valorDescuento + descuento;
+            break;
+    }
+    const textoSocial = document.getElementById("textoSocial")
+    if(precioOriginal >= 0 && valorDescuento >= 0){
+        const precioTotal = calcularPrecioConDescuento(precioOriginal, valorDescuento);
+        textoSocial.innerText = `El total a pagar es de $${precioTotal}`
+    }
 }
+
+
+const quintana = {
+    nombre: "Kevin",
+    apellido: "Quintana",
+    edad: 13,
+    clasesAprobadas: [
+        "Matemáticas",
+        "Lenguaje",
+        "Ingles"
+    ],
+    aprobarCurso(claseAprobada){
+        this.clasesAprobadas.push(claseAprobada)
+    }
+}
+function estudiante(nombre,edad,clasesAprobadas){
+    this.nombre = nombre;
+    this.edad = edad;
+    this.clasesAprobadas = clasesAprobadas;
+}
+estudiante.prototype.aprobarClase = function(claseAprobada){
+    this.clasesAprobadas.push(claseAprobada)
+}
+var nis = new estudiante("Nicolas Sua Cruz", 16,["Ingles", "Matemáticas", "Lenguaje", "Ingles", "Sociales"])
